c5b5c6c7139e4e40d30eac5bd9731d5b2e6f3d66
Elina_Sharlai <--elinakometa@gmail.com>
2025-03-06 12:06:28 +0200

    added new tasks

---
+++ modul_8/lesson-01/lesson-01/js/01-bubbling.js
@@ -0,0 +1,40 @@
+/**
+ * Спливання подій
+ * event.target - цільовий (вихідний) елемент
+ * event.currentTarget - поточний елемент, на слухачі якого спіймали подію
+ */
+
+const parent = document.querySelector("#parent");
+const child = document.querySelector("#child");
+const innerChild = document.querySelector("#inner-child");
+
+// parent.addEventListener("click", event => {
+//   console.log("target", event.target);
+//   console.log("currentTarget", event.currentTarget);
+// });
+parent.addEventListener(
+  "click",
+  event => {
+    console.log("Worked on PARENT");
+  },
+//   true
+);
+
+// child.addEventListener("click", event => {
+//   console.log("target", event.target);
+//   console.log("currentTarget", event.currentTarget);
+// });
+child.addEventListener(
+  "click",
+  event => {
+    console.log("Worked on CHILD");
+  },
+//   true
+);
+
+innerChild.addEventListener("click", event => {
+  event.stopPropagation();
+  console.log("target", event.target);
+
+  console.log("currentTarget", event.currentTarget);
+});
---
+++ modul_8/lesson-01/lesson-01/js/02-delegation.js
@@ -0,0 +1,47 @@
+/**
+ * Додавання прослуховувача подій на кожен елемент
+ *
+ * Отримай колір квадрата по якому було здійснено клік
+ */
+
+const container = document.querySelector(".container");
+// [...container.children].forEach(box => {
+//   box.addEventListener("click", handlerClick);
+// });
+
+// function handlerClick(event) {
+//   const box = event.currentTarget;
+//   console.log("box color:", box.dataset.color);
+//   const colorValue = getComputedStyle(box).getPropertyValue("background-color");
+//   console.log("color value", colorValue);
+// }
+
+/**
+ * Дегегування подій
+ *
+ * Отримай колір квадрата по якому було здійснено клік
+ */
+container.addEventListener("click", onClick);
+
+function onClick(event) {
+  const elem = event.target;
+
+  if (!elem.classList.contains("box")) {
+    return;
+  }
+
+  console.log(" onClick elem:", elem);
+  const color = elem.dataset.color;
+  console.log(" onClick color:", color);
+}
+
+
+
+
+
+
+
+
+
+
+
